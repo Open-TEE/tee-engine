5181bc5b4feb0a495e84175d09d7a19df088f044
Brian McGillion <brian.mcgillion@intel.com>
2013-11-19 18:42:43 +0200

    Fix the license in code and update checkpatch accordingly
    Signed-off-by: Brian McGillion <brian.mcgillion@intel.com>

--- core/main.c
+++ core/main.c
@@ -1,3 +1,19 @@
+/*****************************************************************************
+** Copyright (C) 2013 Intel Corporation.                                    **
+**                                                                          **
+** Licensed under the Apache License, Version 2.0 (the "License");          **
+** you may not use this file except in compliance with the License.         **
+** You may obtain a copy of the License at                                  **
+**                                                                          **
+**      http://www.apache.org/licenses/LICENSE-2.0                          **
+**                                                                          **
+** Unless required by applicable law or agreed to in writing, software      **
+** distributed under the License is distributed on an "AS IS" BASIS,        **
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. **
+** See the License for the specific language governing permissions and      **
+** limitations under the License.                                           **
+*****************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -111,7 +127,7 @@ static void child_main_loop(int childfd)
 	ssize_t n;
 	char buff[1024];
 
-	while((n = (read(childfd, buff, sizeof(buff) -1))) > 0) {
+	while ((n = (read(childfd, buff, sizeof(buff) - 1))) > 0) {
 		buff[n] = '\0';
 		syslog(LOG_DEBUG, "%s", buff);
 	}
