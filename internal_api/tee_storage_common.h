65f145af3b7d86017597ecd032c6415f89bbfcbd
Tanel Dettenborn <dettenbo@cs.helsinki.fi>
2014-01-22 09:20:35 +0200

    Inilization of storage api header file and implemented Generic Object functions, Transient Object functions, Persistant object functions, Data stream functions and Enumeraton functions.
    Change-Id: I1b5fa72f4cbe67a3843171535d36890eb9edc410
    Signed-off-by: Tanel Dettenborn <dettenbo@cs.helsinki.fi>

--- internal_api/tee_storage_common.h
+++ internal_api/tee_storage_common.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
-** Copyright (C) 2013 Intel Corporation.                                    **
+** Copyright (C) 2013 Secure Systems Group.                                 **
 **                                                                          **
 ** Licensed under the Apache License, Version 2.0 (the "License");          **
 ** you may not use this file except in compliance with the License.         **
@@ -14,14 +14,33 @@
 ** limitations under the License.                                           **
 *****************************************************************************/
 
-#ifndef __TEE_INTERNAL_API_EMU_H__
-#define __TEE_INTERNAL_API_EMU_H__
+#ifndef __TEE_OBJECT_ENUM_HANDLE_H__
+#define __TEE_OBJECT_ENUM_HANDLE_H__
 
-#include "../internal_api/data_types.h"
-#include "../internal_api/tee_memory.h"
-#include "../internal_api/tee_ta_interface.h"
-#include "../internal_api/time_api.h"
-#include "../internal_api/storage_data_key_api.h"
-#include "../internal_api/tee_panic.h"
+#include "data_types.h"
 
-#endif
+struct enumerator_data_chunk {
+	char obj_id[TEE_OBJECT_ID_MAX_LEN + 1];
+	size_t obj_id_len;
+	/* Required to fill object info */
+	uint32_t objectType;
+	uint32_t objectSize;
+	uint32_t maxObjectSize;
+	uint32_t objectUsage;
+	uint32_t handleFlags;
+	long dataSize;
+};
+
+struct storage_obj_meta_data {
+	char obj_id[TEE_OBJECT_ID_MAX_LEN + 1];
+	size_t obj_id_len;
+	uint32_t attrs_count;
+	uint32_t meta_size;
+	/* Required from object info */
+	uint32_t objectType;
+	uint32_t maxObjectSize;
+	uint32_t objectUsage;
+	uint32_t handleFlags;
+};
+
+#endif /* __TEE_OBJECT_ENUM_HANDLE_H__ */
