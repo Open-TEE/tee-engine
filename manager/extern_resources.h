f1e1ee8ec901c5624abc3e6be3b1767bf18fec3e
TanelDettenborn <tanel.dettenborn@intel.com>
2014-12-05 12:34:35 +0200

    CA is informed about TA panic if its happen during opensess/invoke call
    Change-Id: Iea048b43ca965fcef733a1c92687260a4ef9c823
    Signed-off-by: TanelDettenborn <tanel.dettenborn@intel.com>

--- manager/extern_resources.h
+++ manager/extern_resources.h
@@ -24,6 +24,10 @@
 #include "tee_list.h"
 #include "tee_shared_data_types.h"
 
+#define WAIT_NO_MSG_OUT		1
+#define WAIT_OPEN_SESSION_MSG	2
+#define WAIT_INVOKE_MSG		3
+
 /* define an opaque structure to handle the process related information */
 typedef struct __proc *proc_t;
 
@@ -122,6 +126,7 @@ struct __proc {
 			proc_t to;
 			uintptr_t sess_ctx;
 			int session_id;
+			int waiting_response_msg;
 			enum session_status status;
 		} sesLink;
 
