334ab96e900fb8b6048f773de3e3b2ae52693db5
Brian McGillion <brian.mcgillion@intel.com>
2016-09-04 22:45:22 +0300

    streamline crypto and storage, migrate to mbedtls
    Change-Id: Ib7a33cb6e760d37b57e7ceda4cbc0fd3dcd431b9
    Signed-off-by: Brian McGillion <brian.mcgillion@intel.com>

--- manager/opentee_manager_storage_api.h
+++ manager/opentee_manager_storage_api.h
@@ -1,17 +1,17 @@
 /*****************************************************************************
-** Copyright (C) 2015 Intel Corporation                                     **
-**                                                                          **
-** Licensed under the Apache License, Version 2.0 (the "License");          **
-** you may not use this file except in compliance with the License.         **
-** You may obtain a copy of the License at                                  **
-**                                                                          **
-**      http://www.apache.org/licenses/LICENSE-2.0                          **
-**                                                                          **
-** Unless required by applicable law or agreed to in writing, software      **
-** distributed under the License is distributed on an "AS IS" BASIS,        **
+** Copyright (C) 2015 Intel Corporation					    **
+**									    **
+** Licensed under the Apache License, Version 2.0 (the "License");	    **
+** you may not use this file except in compliance with the License.	    **
+** You may obtain a copy of the License at				    **
+**									    **
+**	http://www.apache.org/licenses/LICENSE-2.0			    **
+**									    **
+** Unless required by applicable law or agreed to in writing, software	    **
+** distributed under the License is distributed on an "AS IS" BASIS,	    **
 ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. **
-** See the License for the specific language governing permissions and      **
-** limitations under the License.                                           **
+** See the License for the specific language governing permissions and	    **
+** limitations under the License.					    **
 *****************************************************************************/
 
 #ifndef __OPENTEE_MANAGER_STORAGE_API_H__
@@ -23,13 +23,17 @@
 extern TEE_UUID current_TA_uuid;
 
 /* object handling */
-TEE_Result MGR_TEE_OpenPersistentObject(uint32_t storageID, void *objectID, size_t objectIDLen,
-					uint32_t flags, TEE_ObjectHandle *object);
-void MGR_TEE_CloseObject(TEE_ObjectHandle object);
+TEE_Result MGR_TEE_OpenPersistentObject(uint32_t storageID,
+					void *objectID,
+					uint32_t objectIDLen,
+					uint32_t flags);
+
+void MGR_TEE_CloseObject(uint32_t ss_id);
+
 TEE_Result MGR_TEE_CreatePersistentObject(uint32_t storageID, void *objectID, uint32_t objectIDLen,
-					  uint32_t flags, TEE_ObjectHandle attributes,
-					  void *initialData, size_t initialDataLen,
-					  TEE_ObjectHandle *object);
+					  uint32_t flags, TEE_Attribute *attrs,
+					  uint32_t attrs_count, uint8_t persistent_type,
+					  uint32_t *ss_id, TEE_ObjectInfo *info);
 TEE_Result MGR_TEE_RenamePersistentObject(TEE_ObjectHandle object,
 					  void *newObjectID,
 					  uint32_t newObjectIDLen);
