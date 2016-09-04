83e61172573753b4a1b3a8f5d13b29eb74b6292c
Brian McGillion <brian.mcgillion@intel.com>
2016-09-04 22:45:22 +0300

    streamline crypto and storage, migrate to mbedtls
    Change-Id: Ib7a33cb6e760d37b57e7ceda4cbc0fd3dcd431b9
    Signed-off-by: Brian McGillion <brian.mcgillion@intel.com>

---
+++ internal_api/crypto/crypto_digest.h
@@ -0,0 +1,27 @@
+/*****************************************************************************
+** Copyright (C) 2015 Open-TEE project.	                                    **
+**                                                                          **
+** Licensed under the Apache License, Version 2.0 (the "License");          **
+** you may not use this file except in compliance with the License.         **
+** You may obtain a copy of the License at                                  **
+**                                                                          **
+**      http://www.apache.org/licenses/LICENSE-2.0                          **
+**                                                                          **
+** Unless required by applicable law or agreed to in writing, software      **
+** distributed under the License is distributed on an "AS IS" BASIS,        **
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. **
+** See the License for the specific language governing permissions and      **
+** limitations under the License.                                           **
+*****************************************************************************/
+
+#ifndef __CRYPTO_DIGEST_H__
+#define __CRYPTO_DIGEST_H__
+
+#include "../tee_crypto_api.h"
+#include "../../include/tee_shared_data_types.h"
+
+TEE_Result init_gp_digest(TEE_OperationHandle operation, uint32_t algorithm);
+
+void free_gp_digest(TEE_OperationHandle operation);
+
+#endif
---
+++ internal_api/storage/object_handle.h
@@ -0,0 +1,39 @@
+/*****************************************************************************
+** Copyright (C) 2015 Open-TEE project.	                                    **
+**                                                                          **
+** Licensed under the Apache License, Version 2.0 (the "License");          **
+** you may not use this file except in compliance with the License.         **
+** You may obtain a copy of the License at                                  **
+**                                                                          **
+**      http://www.apache.org/licenses/LICENSE-2.0                          **
+**                                                                          **
+** Unless required by applicable law or agreed to in writing, software      **
+** distributed under the License is distributed on an "AS IS" BASIS,        **
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. **
+** See the License for the specific language governing permissions and      **
+** limitations under the License.                                           **
+*****************************************************************************/
+
+#ifndef __OBJECT_HANDLE_H__
+#define __OBJECT_HANDLE_H__
+
+#include <stdio.h>
+#include "../tee_storage_api.h"
+#include "../crypto/operation_handle.h"
+
+struct persistant_object {
+	char obj_id[TEE_OBJECT_ID_MAX_LEN + 1];
+	size_t obj_id_len;
+	FILE *file;
+	uint32_t data_begin;
+	uint32_t data_size;
+	uint32_t data_position;
+};
+
+struct __TEE_ObjectHandle {
+	struct persistant_object per_object;
+	TEE_ObjectInfo objectInfo;
+	struct gp_key *key;
+};
+
+#endif /* __OBJECT_HANDLE_H__ */
